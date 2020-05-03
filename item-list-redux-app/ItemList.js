8cb43de2c92edb4ce2896be1cb7cf8074c1c83e9
casdidier <cheung.didier@gmail.com>
2020-05-03 07:48:43 +0200

    addind a real api call

--- item-list-redux-app/ItemList.js
+++ item-list-redux-app/ItemList.js
@@ -3,28 +3,31 @@ class ItemList extends Component {
     constructor() {
         super();
         this.state = {
-            items: [
-                {
-                    id   : 1,
-                    label: 'List item 1'
-                },
-                {
-                    id   : 2,
-                    label: 'List item 2'
-                },
-                {
-                    id   : 3,
-                    label: 'List item 3'
-                },
-                {
-                    id   : 4,
-                    label: 'List item 4'
-                }
-            ],
+            items     : [],
             hasErrored: false,
             isLoading : false
         };
+
+
+        fetchData(url) {
+            this.setState({ isLoading: true });
+            fetch(url)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw Error(response.statusText);
+                    }
+                    this.setState({ isLoading: false });
+                    return response;
+                })
+                .then((response) => response.json())
+                .then((items) => this.setState({ items })) // ES6 property value shorthand for { items: items }
+                .catch(() => this.setState({ hasErrored: true }));
+        }
     }
+    componentDidMount() {
+        this.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
+    }
+
     render() {
         if (this.state.hasErrored) {
             return <p>Sorry! There was an error loading the items</p>;
