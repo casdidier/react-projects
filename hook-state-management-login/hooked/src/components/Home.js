54b90b0ed01d9e60da0c1484f84594f62360e045
casdidier <cheung.didier@gmail.com>
2020-10-19 18:19:57 +0200

    useEffect to handle network req + token passed by useContext

--- hook-state-management-login/hooked/src/components/Home.js
+++ hook-state-management-login/hooked/src/components/Home.js
@@ -31,22 +31,56 @@ const reducer = (state, action) => {
   }
 };
 export const Home = () => {
+  const { state: authState } = React.useContext(AuthContext);
   const [state, dispatch] = React.useReducer(reducer, initialState);
-return (
-    <div className="home">
-      {state.isFetching ? (
-        <span className="loader">LOADING...</span>
-      ) : state.hasError ? (
-        <span className="error">AN ERROR HAS OCCURED</span>
-      ) : (
-        <>
-          {state.songs.length > 0 &&
-            state.songs.map(song => (
-              <Card key={song.id.toString()} song={song} />
-            ))}
-        </>
-      )}
-    </div>
+  React.useEffect(() => {
+    dispatch({
+      type: "FETCH_SONGS_REQUEST"
+    });
+    fetch("https://hookedbe.herokuapp.com/api/songs", {
+      headers: {
+        Authorization: `Bearer ${authState.token}`
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw res;
+        }
+      })
+      .then(resJson => {
+        console.log(resJson);
+        dispatch({
+          type: "FETCH_SONGS_SUCCESS",
+          payload: resJson
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch({
+          type: "FETCH_SONGS_FAILURE"
+        });
+      });
+  }, [authState.token]);
+
+  return (
+    <React.Fragment>
+      <div className="home">
+        {state.isFetching ? (
+          <span className="loader">LOADING...</span>
+        ) : state.hasError ? (
+          <span className="error">AN ERROR HAS OCCURED</span>
+        ) : (
+              <>
+                {state.songs.length > 0 &&
+                  state.songs.map(song => (
+                    <Card key={song.id.toString()} song={song} />
+                  ))}
+              </>
+            )}
+      </div>
+    </React.Fragment>
   );
 };
 export default Home;
