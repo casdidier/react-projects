53e150f9222c19c8c1b8cfb8b37b7cb1cb8742b7
casdidier <cheung.didier@gmail.com>
2020-10-19 17:58:57 +0200

    added reducer + display conditions

--- hook-state-management-login/hooked/src/components/Home.js
+++ hook-state-management-login/hooked/src/components/Home.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { AuthContext } from "../App";
+import Card from "./Card";
+const initialState = {
+  songs: [],
+  isFetching: false,
+  hasError: false,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_SONGS_REQUEST":
+      return {
+        ...state,
+        isFetching: true,
+        hasError: false
+      };
+    case "FETCH_SONGS_SUCCESS":
+      return {
+        ...state,
+        isFetching: false,
+        songs: action.payload
+      };
+    case "FETCH_SONGS_FAILURE":
+      return {
+        ...state,
+        hasError: true,
+        isFetching: false
+      };
+    default:
+      return state;
+  }
+};
 export const Home = () => {
-  return (
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+return (
     <div className="home">
+      {state.isFetching ? (
+        <span className="loader">LOADING...</span>
+      ) : state.hasError ? (
+        <span className="error">AN ERROR HAS OCCURED</span>
+      ) : (
+        <>
+          {state.songs.length > 0 &&
+            state.songs.map(song => (
+              <Card key={song.id.toString()} song={song} />
+            ))}
+        </>
+      )}
     </div>
   );
 };
