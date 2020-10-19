bd2cb90e00924b171e1cb5ebefcb102fde686ab9
casdidier <cheung.didier@gmail.com>
2020-10-19 17:33:55 +0200

    add the necessary elements for the form

--- hook-state-management-login/hooked/src/components/Login.js
+++ hook-state-management-login/hooked/src/components/Login.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { AuthContext } from "../App";
 export const Login = () => {
   return (
     <div className="login-container">
       <div className="card">
         <div className="container">
+          <form>
+            <h1>Login</h1>
+
+            <label htmlFor="email">
+              Email Address
+              <input
+                type="text"
+                name="email"
+                id="email"
+              />
+            </label>
+
+            <label htmlFor="password">
+              Password
+              <input
+                type="password"
+                name="password"
+                id="password"
+              />
+            </label>
+
+            <button>
+              "Login"
+            </button>
+
+          </form>
         </div>
       </div>
     </div>
