8d07cf01aa044d19720efb42a2f9bc6e3099ca7e
casdidier <cheung.didier@gmail.com>
2020-10-19 17:26:55 +0200

    move to components + authContext export

--- hook-state-management-login/hooked/src/App.js
+++ hook-state-management-login/hooked/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
+export const AuthContext = React.createContext(); // added this
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          new app
-        </p>
-      </header>
-    </div>
+    <AuthContext.Provider>
+      <div className="App"></div>
+    </AuthContext.Provider>
   );
 }
-
 export default App;
