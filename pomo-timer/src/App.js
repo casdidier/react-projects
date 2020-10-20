1d89ce2ef7a962e4fa08c08d86ddfe50fc6c2d27
casdidier <cheung.didier@gmail.com>
2020-10-20 22:20:28 +0200

    handle stop timer with a flag

--- pomo-timer/src/App.js
+++ pomo-timer/src/App.js
@@ -5,23 +5,31 @@ export default function App() {
 
   const [time, setTime] = useState(25 * 60);
   const [title, setTitle] = useState('Start one round');
+  const [isRunning, setIsRunning] = useState(false);
   const intervalRef = useRef(null);
 
 
   const timer = () => setTime(time => time += 1)
 
   function runTimer() {
-    intervalRef.current = setInterval(() => {
-      setTime(time => {
-        if (time >= 1) return time - 1;
-        resetTimer();
-        return 0;
-      });
-    }, 1000);
-
+    if (isRunning) {
+      setIsRunning(false);
+      clearInterval(intervalRef.current);
+    }
+    else {
+      setIsRunning(true);
+      intervalRef.current = setInterval(() => {
+        setTime(time => {
+          if (time >= 1) return time - 1;
+          resetTimer();
+          return 0;
+        });
+      }, 1000);
+    }
   }
 
   const resetTimer = () => {
+    setIsRunning(false);
     clearInterval(intervalRef.current);
     intervalRef.current = null;
     setTime(25 * 60);
@@ -40,7 +48,7 @@ export default function App() {
         <h2>{title}</h2>
         <div className="timer">{`${minutes}:${seconds}`}</div>
         <div className="buttons">
-          <div className="button" onClick={runTimer}>Start</div>
+          <div className="button" onClick={runTimer}>{!isRunning ? 'Start' : 'Pause'}</div>
           <div className="button" onClick={resetTimer}>Reset</div>
         </div>
       </div>
