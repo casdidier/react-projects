47be4152dbcaa5f1d7153381472ac3fa61273792
casdidier <cheung.didier@gmail.com>
2020-10-20 19:15:24 +0200

    timer set up with useRef to manage the setInterval

--- pomo-timer/src/App.js
+++ pomo-timer/src/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./styles.css";
 
 export default function App() {
+
+  const [time, setTime] = useState(2000);
+  const [title, setTitle] = useState('Start one round');
+  const intervalRef = useRef(null);
+
+
+  const timer = () => setTime(time : time += 1)
+
+  function runTimer() {
+    intervalRef.current = setInterval(() => {
+      setTime(time => {
+        if (time >= 1) return time - 1;
+        resetTimer();
+        return 0;
+      });
+    }, 1000);
+
+  }
+
+  const resetTimer = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTime(0);
+  }
+
   return (
     <>
       <div className="app">
-        <h2>Title</h2>
-        <div className="timer">00:00</div>
+        <h2>{title}</h2>
+        <div className="timer">{time}</div>
         <div className="buttons">
-          <div className="button">Start</div>
-          <div className="button">Reset</div>
+          <div className="button" onClick={runTimer}>Start</div>
+          <div className="button" onClick={resetTimer}>Reset</div>
         </div>
       </div>
     </>
