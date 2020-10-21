e6ba41a382738e9c98df881e3d64d69f895da31e
casdidier <cheung.didier@gmail.com>
2020-10-21 11:26:44 +0200

    scaffolding the project

---
+++ paper-scissors/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import Rock from "./icons/Rock";
+import Paper from "./icons/Paper";
+import Scissors from "./icons/Scissors";
+import "./styles.css";
+
+export default function App() {
+
+
+  return (
+    <>
+      <div className="app">
+
+      </div>
+    </>
+  )
+}
