5d2ff93d13148a411b7d74cab990f45ae70f1ed4
casdidier <cheung.didier@gmail.com>
2020-10-24 22:04:21 +0200

    added context to hold other pages states + router for navigation between forms

--- multi-step-form/src/components/SignupForm.js
+++ multi-step-form/src/components/SignupForm.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { SignupFormProvider } from './SignupFormContext';
+import ProfileForm from './ProfileForm';
+import SocialForm from './SocialForm';
+import Review from './Review';
+import StepLinks from './StepLinks';
+
 
 export default function SignupForm() {
+
+  const location = useLocation();
+
+
   return (
-    <div>
-      signupform
-    </div>
+    <SignupFormProvider>
+      <div className="signup-form">
+        <StepLinks />
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact component={ProfileForm} />
+          <Route path="/social" component={SocialForm} />
+          <Route path="/review" component={Review} />
+        </Switch>
+      </div>
+    </SignupFormProvider>
   )
 }
