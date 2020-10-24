907b0dec9677d36f2d5329f70be8056aa646817e
casdidier <cheung.didier@gmail.com>
2020-10-24 23:16:40 +0200

    review final set

--- multi-step-form/src/components/Review.js
+++ multi-step-form/src/components/Review.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useSignupForm } from './SignupFormContext';
 
 export default function Review() {
+  const { profile, social } = useSignupForm();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('you are submitting! congrats!');
+  }
+
   return (
-    <div>
-      Review
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      <h2>Review all your info</h2>
+
+      <p>
+        <strong>Name</strong>: {profile.name}
+      </p>
+      <p>
+        <strong>Email</strong>: {profile.email}
+      </p>
+      <p>
+        <strong>Twitter</strong>: {social.twitter}
+      </p>
+      <p>
+        <strong>Facebook</strong>: {social.facebook}
+      </p>
+
+      <input type="submit" value="Submit All Info" />
+    </form>
+  );
 }
