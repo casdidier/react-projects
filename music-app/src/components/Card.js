10a28ad34060a6df6c8bf91759a33479a6a36b61
casdidier <cheung.didier@gmail.com>
2020-11-26 17:45:37 +0100

    Card component

---
+++ music-app/src/components/Card.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+export const Card = ({ song }) => {
+  return (
+    <div className="card">
+      <img src={song.albumArt} alt="" />
+      <div className="content">
+        <h2>{song.name}</h2>
+        <span>BY: {song.artist}</span>
+      </div>
+    </div>
+  );
+};
+export default Card;
