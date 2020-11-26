e69bba4a082c50d1817b832d98e242f6e6d10be6
casdidier <cheung.didier@gmail.com>
2020-11-26 17:46:09 +0100

    Login component

--- music-app/src/components/Login.js
+++ music-app/src/components/Login.js
@@ -1,24 +1,69 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable react/button-has-type */
 import React from 'react';
+import { AuthContext } from '../App';
 
 export const Login = () => {
+  const { dispatch } = React.useContext(AuthContext);
   const initialState = {
     email: '',
     password: '',
     isSubmitting: false,
     errorMessage: null,
   };
+
   const [data, setData] = React.useState(initialState);
+
   const handleInputChange = (event) => {
     setData({
       ...data,
       [event.target.name]: event.target.value,
     });
   };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setData({
+      ...data,
+      isSubmitting: true,
+      errorMessage: null,
+    });
+    fetch('https://music-server-react.herokuapp.com/api/login', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: data.email,
+        password: data.password,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw res;
+      })
+      .then((resJson) => {
+        dispatch({
+          type: 'LOGIN',
+          payload: resJson,
+        });
+      })
+      .catch((error) => {
+        setData({
+          ...data,
+          isSubmitting: false,
+          errorMessage: error.message || error.statusText,
+        });
+      });
+  };
+
   return (
     <div className="login-container">
       <div className="card">
         <div className="container">
-          <form>
+          <form onSubmit={handleFormSubmit}>
             <h1>Login</h1>
 
             <label htmlFor="email">
@@ -48,7 +93,11 @@ export const Login = () => {
             )}
 
             <button disabled={data.isSubmitting}>
-              {data.isSubmitting ? 'Loading...' : 'Login'}
+              {data.isSubmitting ? (
+                <img className="spinner" alt="loading icon" />
+              ) : (
+                'Login'
+              )}
             </button>
           </form>
         </div>
@@ -56,4 +105,5 @@ export const Login = () => {
     </div>
   );
 };
+
 export default Login;
