f9e53ed6df8c6022769fed19fd7ede5e01dcbb3f
casdidier <cheung.didier@gmail.com>
2020-11-27 11:41:46 +0100

    handle Logout action

--- music-app/src/components/Header.js
+++ music-app/src/components/Header.js
@@ -1,6 +1,26 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable import/no-cycle */
 import React from 'react';
+import { AuthContext } from '../App';
 
-export const Home = () => {
-  return <div className="home" />;
+export const Header = () => {
+  const { state, dispatch } = React.useContext(AuthContext);
+  return (
+    <nav id="navigation">
+      <h1 href="#" className="logo">
+        HOOKED
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'LOGOUT',
+          })
+        }
+      >
+        {state.isAuthenticated && <h1>Hi {state.user.firstName} (LOGOUT)</h1>}
+      </button>
+    </nav>
+  );
 };
-export default Home;
+
+export default Header;
